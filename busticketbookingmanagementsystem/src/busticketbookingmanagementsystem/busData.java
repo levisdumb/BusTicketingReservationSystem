fedccfecb84e63bfff868e5f0a79f0ab77c444f4
levisdumb <118234585+levisdumb@users.noreply.github.com>
2023-05-11 11:00:43 +0800

    Add files via upload

---
+++ busticketbookingmanagementsystem/src/busticketbookingmanagementsystem/busData.java
@@ -0,0 +1,38 @@
+
+package busticketbookingmanagementsystem;
+
+import java.sql.Date;
+
+
+public class busData {
+    
+    private Integer busId;
+    private String location;
+    private String status;
+    private Double price;
+    private Date date;
+    
+    public busData(Integer busId, String location, String status, Double price, Date date){
+        this.busId = busId;
+        this.location = location;
+        this.status = status;
+        this.price = price;
+        this.date = date;
+    }
+    
+    public Integer getBusId(){
+        return busId;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public Double getPrice(){
+        return price;
+    }
+    public Date getDate(){
+        return date;
+    }
+}
---
+++ busticketbookingmanagementsystem/src/busticketbookingmanagementsystem/getData.java
@@ -0,0 +1,10 @@
+
+package busticketbookingmanagementsystem;
+
+
+public class getData {
+    
+    public static Integer number;
+    public static String username;
+    
+}
